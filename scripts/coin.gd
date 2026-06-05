b6efb5cc977a5123e6bbf045c7f074011e7bc221
lucben000 <lucben000@gmail.com>
2026-06-04 17:59:02 -0700

    added music/sfx and added gravity to enemies

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,8 +2,9 @@ extends Area2D
 
 
 @onready var score_label: Label = %ScoreLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
 	score_label.add_score()
-	queue_free()
+	animation_player.play("pickup")
--- scripts/green_slime_enemy.gd
+++ scripts/green_slime_enemy.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 var speed = 60
 var direction = 1
+var gravity = 150
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_down: RayCast2D = $RayCastDown
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	if ray_cast_down.is_colliding() == false:
+		position.y += gravity * delta
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +25,4 @@ func _process(delta: float) -> void:
 		direction = 1
 		animated_sprite_2d.flip_h = false
 	position.x += direction * speed * delta
+	
