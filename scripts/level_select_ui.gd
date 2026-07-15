19b64ca0113e8cf83161f845091bb30f961d57d3
lucben000 <lucben000@gmail.com>
2026-07-14 18:00:51 -0700

    added locked levels and new asset to show it, fixed ui, able to press levels if unlocked, and transition to level 1 from the level ui

--- scripts/level_select_ui.gd
+++ scripts/level_select_ui.gd
@@ -2,12 +2,49 @@ extends Control
 
 @export var level_select_ui: Control
 var menu: Control
+var tutorial: Node
+var camera: Camera2D
+
+@export var label_1: Label
+@export var label_2: Label
+@export var label_3: Label
+@export var label_4: Label
+@export var label_5: Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	menu = preload("res://scenes/menu_ui.tscn").instantiate()
+	menu = load("res://scenes/menu_ui.tscn").instantiate()
+	tutorial = preload("res://scenes/level_1.tscn").instantiate()
+	label_1.modulate.a = 0.5
+	label_2.modulate.a = 0.5
+	label_3.modulate.a = 0.5
+	label_4.modulate.a = 0.5
+	label_5.modulate.a = 0.5
+	camera = get_parent().get_node("Camera2D")
 
 func _on_back_button_pressed() -> void:
 	get_parent().add_child(menu)
 	level_select_ui.queue_free()
 	print("back to menu")
+
+func _on_tutorial_pressed() -> void:
+	get_parent().add_child(tutorial)
+	get_parent().remove_child(level_select_ui)
+	camera.queue_free()
+	print("tutorial")
+
+func _on_level_1_pressed() -> void:
+	print("level 1")
+
+func _on_level_2_pressed() -> void:
+	print("level 2")
+
+func _on_level_3_pressed() -> void:
+	print("level 3")
+
+func _on_level_4_pressed() -> void:
+	print("level 4")
+
+func _on_level_5_pressed() -> void:
+	print("level 5")
